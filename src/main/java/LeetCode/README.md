0d7b7b1cc9b733530a7c8acb56eeede3918cd169
CstomRita <cstvita@163.com>
2018-09-20 20:46:02 +0800

    FirstCommit

---
+++ src/main/java/LeetCode/README.md
@@ -0,0 +1,9 @@
+## 这里是LeetCode刷题过程中的记录
+
+#### nnums 
+
+一个数组中，N个数相加为target，返回这些情况
+
+#### SlidingWindows
+
+滑动窗口
---
+++ src/main/java/LeetCode/SlidingWindows/Solution.java
@@ -0,0 +1,22 @@
+package LeetCode.SlidingWindows;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @ Author     ：CstomRita
+ * @ Date       ：Created in 下午3:37 2018/9/11
+ * @ Description：
+ * @ Modified By：
+ * @Version: $
+ */
+public class Solution {
+    // leetcode 第三题
+//    public int LengthOfLongestSubstring(String s) {
+//
+//    }
+    public static void main(String[] args) {
+        Set set =  new HashSet();
+        set.add(1);
+    }
+}
